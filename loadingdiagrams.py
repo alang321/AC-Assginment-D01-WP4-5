021c16db6194993378547502bcfd7407ffba6382
OHeukelom <71064540+OHeukelom@users.noreply.github.com>
2020-11-12 14:46:09 +0100

    Create loadingdiagrams.py

---
+++ loadingdiagrams.py
@@ -0,0 +1,24 @@
+from aircraftProperties import AircraftProperties
+
+
+###---constants---###
+
+
+
+#altitudes [m]
+h_0 = 0         #altitude at sea level 
+h_1 =   2        # ????
+h_c = 9448      #altitude at cruise 
+
+#weights [N]
+OEW = AircraftProperties.Weight["OEW"]   #operating empty weight
+ZFW = 1539078  #zero fuel weight
+MTOW = 2014954 # maximum takeoff weight
+
+#V_s0 =  
+#V_s1 = 
+#V_A = 
+#V_C = 
+#V_D = 
+
+print(OEW)
