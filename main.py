276cae5e3efb435040bf9e8ad6096eab845551bf
Tim <tim-hogenelst@hotmail.com>
2020-11-16 14:41:52 +0100

    Data processing mk4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from aircraftProperties import AircraftProperties
 from wingboxGeometry import WingboxGeometry
+import AerodynamicDataThingy
 
 print(AircraftProperties.Fuselage["Fuselage length"])
 
@@ -10,3 +11,6 @@ wingbox.drawWingbox()
 
 test = WingboxGeometry(forwardSpar=0.087654, aftSpar=0.87)
 test.drawWingbox()
+
+print(AerodynamicDataThingy.Dragacc(10))
+AerodynamicDataThingy.drawgraphs()
