2d0391964cddffc67030472c8cd3b7c0731d93f0
alang321 <oesterreich25euro@gmail.com>
2020-11-16 22:40:57 +0100

    some corssesection vhanges

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import AerodynamicDataThingy
 print(AircraftProperties.Fuselage["Fuselage length"])
 
 wingbox = WingboxCrossection(forwardSpar=0.15, aftSpar=0.6, chordLength=10, numStringerTop=8, numStringersBottom=3, stringerInertia=0.000000002238636567, stringerArea=0.00005775, sidewallThickness=[0.1, 0.1, 0.1, 0.1])
-print(wingbox.edgeCoordinates())
-print(wingbox.calculateEnclArea())
+print("Corner Coordinates:", wingbox.getCornerCoordinates())
+print("Enclosed Area:", wingbox.getEnclosedArea())
 wingbox.drawWingbox()
 
 print(AerodynamicDataThingy.Dragacc(10))
--- wingboxCrosssection.py
+++ wingboxCrosssection.py
@@ -6,10 +6,13 @@ import numpy as np
 
 class WingboxCrossection:
 
-    def __init__(self, chordLength, forwardSpar, aftSpar, numStringerTop, numStringersBottom, sidewallThickness, stringerInertia, stringerArea): # sidewall thickness = [top, right, bottom, left]
+    # sidewall thickness = [left, bottom, right, top], numStringers = [top, bottom]
+    def __init__(self, chordLength, forwardSpar, aftSpar, numStringers, sidewallThickness, stringerArea):
         self.chordLength = chordLength
-        self.numStringerTop = numStringerTop
-        self.numStringersBottom = numStringersBottom
+        self.numStringers = numStringers
+        self.sidewallThickness = sidewallThickness
+
+        self.stringerArea = stringerArea
 
         self.forwardSpar = forwardSpar
         self.aftSpar = aftSpar
@@ -20,22 +23,22 @@ class WingboxCrossection:
 
         self.airfoilData = self.parseAirfoilData()
 
-        self.wingboxCornerCordinates = self.cornerCoordinates()
+        self.cornerCoordinates = self.getCornerCoordinates()
 
-        self.stringerLocations = self.stringerLocations()
+        self.stringerLocations = self.getStringerLocations()
 
         #section Properties
-        self.enclosedArea = self.calculateEnclArea()
-        self.centroid = self.calculateCentroid()
-        self.ixx = self.calculateInertia() #horizontal
-        self.izz = self.calculateInertia() #vertical
-        self.iyy = self.calculateInertia() #polar
+        self.enclosedArea = self.getEnclosedArea()
+        self.centroid = self.getCentroid()
 
+        self.centroid = [0,0]
 
-
+        self.ixx = self.getInertia() #horizontal
+        self.izz = self.getInertia() #vertical
+        self.iyy = self.getInertia() #polar
 
     # uses the forward and aft spar as fraction of chord, returns the coordinates of the wing box corners as a fraction of the chord in a list of tuples,
-    def cornerCoordinates(self):
+    def getCornerCoordinates(self):
         coordinates = []
         for spar in self.spars:
             for intersection in self.intersection(spar):
@@ -44,55 +47,69 @@ class WingboxCrossection:
         return coordinates
 
     # calculate the centroid and returns it ass coordinate
-    def calculateCentroid(self):
+    def getCentroid(self):
         centroid = []
         # centroid of i stringers (area times distance)
         # centroid of trapezoid wingbox
-        coords = self.edgeCoordinates()
+        coords = self.getCornerCoordinates()
         # centroidx = coords[][] - coords[][]
         # centroidy = coords[][] - coords[][]
 
         # average of all centroids = final centroid
         return centroid
 
-    # calculate the centroid and returns it ass coordinate
-    def calculateInertia(self):
+    # calculate the inertia
+    def getInertia(self):
         inertia = 0 #m^4
 
+        sidePanels = [] # centroid coords, angle in rad, length, thickness
+        for index in range(4):
+            nextIndex = (index+1)%4
 
+            coordCentroid = [0, 0]
+            for i in range(2):
+                coordCentroid[i] = (self.cornerCoordinates[index][i] + self.cornerCoordinates[nextIndex][i]) / 2
 
-        #tilted rectanlge
-        #vertical
-        #for each stringer
+            angle = WingboxCrossection.getAngleLineHorizontal(self.cornerCoordinates[index], self.cornerCoordinates[nextIndex])
+            length = WingboxCrossection.getDistanceBetweenPoints(self.cornerCoordinates[index], self.cornerCoordinates[nextIndex])
 
+            sidePanels.append([coordCentroid, angle, length, self.sidewallThickness[index]])
+        print(sidePanels)
 
         return inertia
 
     # calculate the centroid and returns it ass coordinate
-    def calculateStringerLocations(self):
+    def getStringerLocations(self):
         coordinateList = []
-        coords = self.edgeCoordinates()
-        x = coords[3][0]-coords[0][0]
-        dx = x/(self.numStringerTop-1)
+        coords = self.getCornerCoordinates()
+
+
+        for numStringersPerSide in self.numStringers:
+            x = coords[3][0]-coords[0][0]
+            dx = x/(numStringersPerSide-1)
+
+            for stringer in numStringersPerSide:
+                dx * stringer
+
         
         return coordinateList
 
     # calculate the area enclosed by wingbox in terms of chord
-    def calculateEnclArea(self):
-        coords = self.edgeCoordinates()
+    def getEnclosedArea(self):
+        coords = self.getCornerCoordinates()
         fwdspar = coords[0][1] - coords[1][1]
         bckspar = coords[3][1] - coords[2][1]
         lengwingbox = coords[2][0] - coords[0][0]
         A_m = 0.5*lengwingbox*(fwdspar + bckspar)
         return A_m
 
-    def drawWingbox(self):
+    def drawWingbox(self, drawStringerLocations=False):
         #plot top and bottom line
-        plt.plot(self.airfoilData[0][0], self.airfoilData[0][1], color="blue")
-        plt.plot(self.airfoilData[1][0], self.airfoilData[1][1], color="blue")
+        plt.plot([i*self.chordLength for i in self.airfoilData[0][0]], [i*self.chordLength for i in self.airfoilData[0][1]], color="blue")
+        plt.plot([i*self.chordLength for i in self.airfoilData[1][0]], [i*self.chordLength for i in self.airfoilData[1][1]], color="blue")
 
         #plot wingbox
-        coord = self.cornerCoordinates()
+        coord = self.getCornerCoordinates()
         coord.append(coord[0])  # repeat the first point to create a 'closed loop'
         xs, ys = zip(*coord)  # create lists of x and y values
         plt.plot(xs, ys, color="red")
@@ -101,8 +118,8 @@ class WingboxCrossection:
         plt.plot(self.centroid[0], self.centroid[1], marker='o', color='r', ls='')
 
         #x, y ranges and same scale
-        plt.xlim(-0.1, 1.1)
-        plt.ylim(-0.5, 0.5)
+        plt.xlim(-0.1*self.chordLength, 1.1*self.chordLength)
+        plt.ylim(-0.5*self.chordLength, 0.5*self.chordLength)
         plt.gca().set_aspect('equal', adjustable='box')
 
         plt.show()
@@ -115,16 +132,14 @@ class WingboxCrossection:
         return float(f_upper(location)), float(f_lower(location))
 
     @staticmethod #calculate the angle between a line defined by 2 coordinates and the x axis
-    def calculateAngleLineHorizontal(coord1, coord2):
+    def getAngleLineHorizontal(coord1, coord2):
+        if coord2[0] == coord1[0]:
+            return np.pi/2
         return np.arctan((coord2[1]-coord1[1])/(coord2[0]-coord1[0]))
 
-    @staticmethod #calculate the intersection of a line defined by 2 coordinates and a x coordinate
-    def calculateYValueAtXofLine(coord1, coord2, x):
-        # linear function: kx+d
-        k = (coord2[1]-coord1[1])/(coord2[0]-coord1[0])
-        d = coord1[1] - k * coord1[0]
-
-        return k*x+d
+    @staticmethod
+    def getDistanceBetweenPoints(coord1, coord2):
+        return ((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2) ** 0.5
 
     @staticmethod
     def parseAirfoilData():
