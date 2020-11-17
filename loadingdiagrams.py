d54d8c6a8c0a4ffd13d6cbb136ff8742d56a66bc
Mickiboyhekkens <74193325+Mickiboyhekkens@users.noreply.github.com>
2020-11-17 16:56:31 +0100

    Update loadingdiagrams.py

--- loadingdiagrams.py
+++ loadingdiagrams.py
@@ -5,28 +5,53 @@ import math
 
 #altitudes [m]
 h_0 = 0                                                              #sea level 
-rho_0 = 1.225                                                  
-h_1 =     1                                                          # ???? (intermediate altitude)
-rho_1 =  1
+rho_0 = 1.225
+T_0 = 288.15
+h_1 =  4572                                                          # ???? (intermediate altitude)
+rho_1 = 0.770816  
+T_1 = 258.432
 h_c = AircraftProperties.Cruise_constants["cruise altitude"]         #altitude at cruise 
-rho_c = AircraftProperties.Cruise_constants["density at cruise"] 
+rho_c = AircraftProperties.Cruise_constants["density at cruise"]
+T_c = 226.733
+g = 9.81
+
 
 #weights [N]
 MTOW = AircraftProperties.Weight["MTOW"]                             #maximum takeoff weight
 W_pl = AircraftProperties.Weight["payload at harmonic profile"]      #weight payload 
 OEW = AircraftProperties.Weight["OEW"]                               #operating empty weight
 ZFW = OEW + W_pl                                                     #zero fuel weight
+weights_dic = {'OEW': OEW, 'ZFW': ZFW, 'MTOW': MTOW}
 
 #velocities
-V_c = AircraftProperties.Cruise_constants["cruise velocity"]
+M_c = AircraftProperties.Cruise_constants["mach at cruise"]
+
 
 #aircraft properties
 S = AircraftProperties.Planform["surface area"]
 C_L_max_flaps = AircraftProperties.Lift["CL max with flaps"]
 C_L_max = AircraftProperties.Lift["CL max without flaps"]
+C_MAC = AircraftProperties.Planform["MAC"]
+
+
+#maximimum load factors
+n_max = 2.5
+n_min = -1.0
+
+#dictionaries
+rho_h_dic = {'SL': rho_0, 'FL150': rho_1, 'FL310': rho_c}
+T_dic = {'SL': T_0, 'FL150': T_1, 'FL310': T_c}
+h_dic = {'SL': 0, 'FL150': 4572, 'FL310': 9449}
+weights_dic = {'OEW': OEW, 'ZFW': ZFW, 'MTOW': MTOW}
 
 
 ###---velocity calculations---###
+
+def get_V_C(T_h):
+
+    V_C = M_c * math.sqrt(T_h * 402.058)
+
+    return V_C
 
 def get_V_S0(W, rho_h):                                                 #stall speed with flaps
     
@@ -40,11 +65,118 @@ def get_V_S1(W, rho_h):                                                 #stall s
     
     return V_S1
 
+def get_V_A(V_S1):
+
+    V_A = math.sqrt(n_max)*V_S1
+
+    return V_A
+
+def get_V_D(V_C):
+
+    V_D = V_C * 1.25
+
+    return V_D
+
+
+###---aerodynamic calculations---###
+C_L_alpha = 4.62
+
+
+###---gust calculations---###
+
+def get_weight_factor(W, rho_h):
+
+    weight_factor = (2 * W) / (S * rho_h * g * C_MAC * C_L_alpha)
+
+    return weight_factor
+
+def get_K_g(weight_factor):
+
+    K_g = (0.88 * weight_factor) / (5.3 + weight_factor)
+
+    return K_g
+
+def get_U_ref(altitude):
+    if altitude <= 4572:
+        U_ref = 17.07 -0.000801*altitude
+    else:
+        U_ref = 17.07 - (0.000801*4572) - (0.000513998*(altitude-4572))
+    return U_ref
+
+def get_V_B(W, K_g, V_S1):
+
+    V_B = V_S1 * math.sqrt(1+ (K_g * rho_0 * U_ref * V_C * C_L_alpha)/(2*W))
+
+    return V_B
+
+###---speeds results---###
+
+V_C_values = {}
+V_S0_values = {}
+V_S1_values = {}
+V_D_values = {}
+V_A_values = {}
+V_B_values = {}
+
+
+          
+for weights, values in weights_dic.items():
+    for altitudes, temperatures in T_dic.items():
+
+        nametagC = 'V_C {} {}'.format(weights, altitudes)
+        nametagD = 'V_D {} {}'.format(weights, altitudes)
+
+        V_C = get_V_C(temperatures)
+        V_D = get_V_D(V_C)
+
+        V_C_values[nametagC] = V_C
+        V_D_values[nametagD] = V_D
+
+    
+    for altitudes, densitys in rho_h_dic.items():
+
+        nametagS0 = 'V_S0 {} {}'.format(weights, altitudes)
+        nametagS1 = 'V_S1 {} {}'.format(weights, altitudes)
+        nametagA = 'V_A {} {}'.format(weights, altitudes)
+        nametagB = 'V_B {} {}'.format(weights, altitudes)
+
+        V_S0 = get_V_S0(values, densitys)
+        V_S1 = get_V_S1(values, densitys)
+        V_A = get_V_A(V_S1)
+
+        altitude = h_dic[altitudes]
+        U_ref = get_U_ref(altitude)
+        weight_factor = get_weight_factor(values, densitys)
+        K_g = get_K_g(weight_factor)
+        V_B = get_V_B(values, K_g, V_S1)
+        
+        V_S0_values[nametagS0] = V_S0
+        V_S1_values[nametagS1] = V_S1
+        V_A_values[nametagA] = V_A
+        V_B_values[nametagB] = V_B
+        
+        
+        
+     
+print(V_C_values)
+print(V_D_values)
+print(V_S0_values)
+print(V_S1_values)
+print(V_A_values)
+print(V_B_values)
+        
+            
 
 
 
 
 
-a = get_V_S0(MTOW, rho_0)
 
-print(a)
+
+
+
+
+
+
+
+
