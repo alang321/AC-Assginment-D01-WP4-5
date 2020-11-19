0e27326805530d7d2b4be833944e205e67ba78c1
OHeukelom <71064540+OHeukelom@users.noreply.github.com>
2020-11-19 17:24:52 +0100

    Update load diagram generation.py

--- load diagram generation.py
+++ load diagram generation.py
@@ -9,20 +9,21 @@ Created on Mon Nov 16 15:51:54 2020
 
 import numpy as np 
 import matplotlib.pyplot as plt
+import math
 
 ###---MANEUVRE LOAD DIAGRAM---###
 
 #test speeds (inputs)
 
-V_A = 230
-V_D = 300
-V_F = 270
-V_S1 = 200
-V_S0 = 150
+V_A = 78.4
+V_D = 327.6
+V_F = 89.2
+V_S1 = 49.6
+V_S0 = 39.1
 
 #test loads (inputs)
-n_max = 3
-n_min = -1.2
+n_max = 2.5
+n_min = -1
 
 
 speeds = [V_A, V_D, V_D, V_F, V_S1]  
@@ -38,13 +39,20 @@ plt.text(V_A, n_max, 'V_A') #add text to diagram
 def f(x):
     return (x / V_S1)**2
 
-
 x1 = np.linspace(0,V_A, 1000)
+x2 = np.linspace(0, V_S1 * math.sqrt(2), 1000)
+x3 = np.linspace(0, V_S1, 1000
+                 )
+y2 = []      #flaps down curve n values
 
+for i in x2:
+    a = (i / V_S0)**2
+    a = min(a, 2)
+    y2.append(a)
 
-
-plt.plot(x1, (x1 / V_S0)**2, 'black')  # (0,0) to V_A curve
-#plt.plot(x1, min((x1 / V_S0)**2, 2), 'black')  #flaps down curve
+plt.plot(x1, (x1 / V_S1)**2, 'black')  # (0,0) to V_A curve
+plt.plot(x2, y2, 'black')  #flaps down curve
+plt.plot(x3, -(x3 / V_S1)**2, 'black')
  
 plt.show()
 
