760f566265a588383e8a36d4a327dca07c4907ba
OHeukelom <71064540+OHeukelom@users.noreply.github.com>
2020-11-19 15:06:30 +0100

    Update load diagram generation.py

--- load diagram generation.py
+++ load diagram generation.py
@@ -12,24 +12,42 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 
 
-speeds = [0, 230, 300, 300, 270, 210, 150]   # origin, V_A, V_D, V_D, V_F, V_S1, 
-n_values = [0, 3, 3, 0, -1.2, -1.2, 0]
+#test speeds (inputs)
+
+V_A = 230
+V_D = 300
+V_F = 270
+V_S1 = 200
+
+#test loads (inputs)
+n_max = 3
+n_min = -1.2
+
+
+speeds = [0,V_A, V_D, V_D, V_F, V_S1]  
+n_values = [0,n_max, n_max, 0, n_min, n_min]
 
 diagram_values = np.column_stack((speeds, n_values))
 
+maneuvre_envelope = plt.figure()
+
+ax = maneuvre_envelope.add_subplot(1, 1, 1)
+ax.spines['left'].set_position('center')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
 
 for i in range(1, len(speeds)):
-    plt.plot(speeds[i:i+2], n_values[i:i+2], "ro-")
+    plt.plot(speeds[i:i+2], n_values[i:i+2], "black")
+    
+    
     
 plt.show()
 
 
 
-x = [speeds[0],speeds[5],speeds[1]]             
-y=[0,2,n_values[1]]
 
-x2 = np.linspace(x[0], x[-1], 100)
-y2 = interpolate.pchip_interpolate(x, y, x2)
-plt.plot(x2, y2, "ro-")
-plt.plot(x, y, "o")
 
