5f4f65169170e924dfde5fa3a357bbc89f22406b
teunisdebeer <74192416+teunisdebeer@users.noreply.github.com>
2020-11-23 16:27:41 +0100

    Update test0101sothatantonishappy.py
    clalfa

--- test0101sothatantonishappy.py
+++ test0101sothatantonishappy.py
@@ -1,33 +1,10 @@
-g = 9.80665
-R = 287.0
-T0 = 288.15
-p = 101325.0
-a = -0.0065
+from math import sqrt, pi, radians, tan
+from aircraftProperties import AircraftProperties
 
+A = AircraftProperties.Planform["aspect ratio"]
+Beta = sqrt(1-(AircraftProperties.Cruise_constants["mach at cruise"])**2)
+eff = 0.95
+sweep = AircraftProperties.Planform["half-chord sweep"]
 
-ch=float(input('\nEnter your choice:  '))
-
-if (ch == 1):
-
-    print('\n    **** ISA calculator troposhere ****')
-
-    
-    h=float(input('\nEnter altitude [m]    '))
-        
-    if h <= 11000.0:
-        h1=h
-        
-        T1= T0 + (a*h)
-        C1=T1 - 273.15
-        print('\nTemperature:',round(T1,2),'K', '(',round(C1,1),'C)')
-    
-        
-        p1= p*((T1/T0)**(-(g/(a*R))))
-        Sl= (p1/p)*100
-        print('Pressure:',round(p1), '(',round(Sl),'% SL)')
-    
-        
-        rho= p1/(R*T1)
-        Slr= (rho/1.225)*100
-        print('density:',round(rho,4),'(',round(Slr),'% SL)')
-        print('\nReady.')
+y = (2*pi*A)/(2+sqrt(4+(((A*Beta)**2)/eff)*(1+((tan(radians(sweep))**2)/(Beta**2)))))
+print(y)
