8609ae4123865d166fb3bc83e354d6cf99a3484d
OHeukelom <71064540+OHeukelom@users.noreply.github.com>
2020-11-29 17:08:23 +0100

    Update maneuvre_loadingdiagrams.py

--- maneuvre_loadingdiagrams.py
+++ maneuvre_loadingdiagrams.py
@@ -332,17 +332,22 @@ def plot_maneuver(V_A, V_D, V_F, V_S0, V_S1, title = 'Manoeuvre diagram'):
     plt.xlabel('Velocity (m/s)')
     plt.ylabel('Load (n)')
     
-    plt.text(V_A+5, -1.25, 'V_A') #add text to diagram 
-    plt.axvline(x=V_A, color = 'black', linestyle = ':')
+    #plt.text(V_A+5, -1.25, 'V_A') #add text to diagram 
+    plt.axvline(x=V_A, color = 'green', linestyle = '--')
     
-    plt.text(V_S1+5, -1.25, 'V_S1') #add text to diagram 
-    plt.axvline(x=V_S1, color = 'red', linestyle = ':')
+    #plt.text(V_S1+5, -1.25, 'V_S1') #add text to diagram 
+    plt.axvline(x=V_S1, color = 'red', linestyle = '--')
 
-    plt.text(V_D+5, -1.25, 'V_D') #add text to diagram 
-    plt.axvline(x=V_D, color = 'black', linestyle = ':')
+    #plt.text(V_D+5, -1.25, 'V_D') #add text to diagram 
+    plt.axvline(x=V_D, color = 'blue', linestyle = '--')
 
-    plt.text(V_F+5, -1.25, 'V_C') #add text to diagram 
-    plt.axvline(x=V_F, color = 'black', linestyle = ':')
+    #plt.text(V_F+5, -1.25, 'V_C') #add text to diagram 
+    plt.axvline(x=V_F, color = 'orange', linestyle = '--')
+    
+    plt.axvline(x=V_S0, color = 'purple', linestyle = '--')
+    
+    plt.axvline(x=V_C, color = 'yellow', linestyle = '--')
+
     
     plt.text(V_S1 * math.sqrt(2) - 70, 2, 'Flaps') #add text to diagram 
     plt.text(V_S1 * math.sqrt(2) - 70, 1.5, 'Down') #add text to diagram 
@@ -361,7 +366,7 @@ def plot_maneuver(V_A, V_D, V_F, V_S0, V_S1, title = 'Manoeuvre diagram'):
     x2 = np.linspace(0, V_S1 * math.sqrt(2), 1000)
     x3 = np.linspace(0, V_S1, 1000)
     y2 = []      #flaps down curve n values
-
+    
     for i in x2:
         a = (i / V_S0)**2
         a = min(a, 2)
@@ -370,8 +375,7 @@ def plot_maneuver(V_A, V_D, V_F, V_S0, V_S1, title = 'Manoeuvre diagram'):
     plt.plot(x1, f(x1), 'black')  # (0,0) to V_A curve
     plt.plot(x2, y2, 'black')  #flaps down curve
     plt.plot(x3, -f(x3), 'black')
-    plt.axvline(x=V_A, color = 'black', linestyle = '--')
-    plt.axvline(x=V_S0, color = 'black', linestyle = '--')
+    
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.5, hspace=0.5)
     
     plt.show()
